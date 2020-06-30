a9040d701f5479ad9474cdff46034410329cdaeb
Victor Oliveira <victor.oliveira.cs@gmail.com>
2020-06-30 19:09:14 -0300

    Initial Commit

---
+++ EulerStudy/EulerStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerStudyApp.swift
+//  EulerStudy
+//
+//  Created by Victor Oliveira on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct EulerStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
